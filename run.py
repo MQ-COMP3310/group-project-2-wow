5af6f0069b24b211641f31d849163ab0587426c3
Manav <manav.thobhani@gmail.com>
2025-06-07 23:28:03 +1000

    Check if run.py is being executed on a windows machine so that the powershell script call doesn't crash

--- run.py
+++ run.py
@@ -1,15 +1,18 @@
 from project import create_app
+import platform
 import subprocess
 
 # Run create_app() and run the app on localhost (and all other ports)
 if __name__ == '__main__':
   # Install playwright packages
   script_path = 'install_playwright.ps1'
+  # Only run PowerShell install script on Windows
 
-  try:
-    subprocess.run(["powershell", "-ExecutionPolicy", "Bypass", "-File", script_path], check=True)
-  except subprocess.CalledProcessError as e:
-    print(e.stderr)
+  if platform.system() == "Windows":
+    try:
+      subprocess.run(["powershell", "-ExecutionPolicy", "Bypass", "-File", script_path], check=True)
+    except subprocess.CalledProcessError as e:
+      print(e.stderr)
   
   app = create_app()
   app.run(host = '0.0.0.0', port = 8000, debug = True)
