f3d4361e5cd0020cc541d9de0d57c14f0b881c48
Manav <manav.thobhani@gmail.com>
2025-06-07 23:23:08 +1000

    Try/catch for run.py to continue if install_playwright.ps1 throws any errors

--- run.py
+++ run.py
@@ -5,7 +5,11 @@ import subprocess
 if __name__ == '__main__':
   # Install playwright packages
   script_path = 'install_playwright.ps1'
-  subprocess.run(["powershell", "-ExecutionPolicy", "Bypass", "-File", script_path], check=True)
+
+  try:
+    subprocess.run(["powershell", "-ExecutionPolicy", "Bypass", "-File", script_path], check=True)
+  except subprocess.CalledProcessError as e:
+    print(e.stderr)
   
   app = create_app()
   app.run(host = '0.0.0.0', port = 8000, debug = True)
