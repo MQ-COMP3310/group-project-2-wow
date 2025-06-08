f08b82fe2b82602b4df6a38440f447bc7f880bc7
Manav <manav.thobhani@gmail.com>
2025-06-08 23:18:27 +1000

    Update playwright packages

--- install_playwright.ps1
+++ install_playwright.ps1
@@ -1,4 +1,6 @@
 # Part 2 Task 7: Implementation of Authentication
 # Part 3 Task 9: Implementation of Additional Features
+rm -rf node_modules package-lock.json
+npm cache clean --force
 npm install -D @playwright/test
 npx playwright install
